3318ec6e03a273fcb0a7beb34af3400824f8bf44
mutapakkir <151417931+mutapakkir@users.noreply.github.com>
2024-12-15 03:10:04 -0800

    Add files via upload

---
+++ SERVERSISWA/routes/user-routers.js
@@ -0,0 +1,9 @@
+const userContoller = require('../controllers/user-controllers') 
+const express = require('express');
+const router = express.Router();
+
+router.get('/students', userContoller.getAllStudent)
+router.get('/students/:id', userContoller.getStudentById)
+router.post('/', userContoller.addStudent)
+
+module.exports = router;
